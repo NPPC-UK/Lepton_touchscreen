ac5b458f7deec61ccd7693bc0a27626cdd082c52
Colin Sauze <cos@Aber.ac.uk>
2016-03-10 16:27:18 +0000

    12 bit image depth

--- LeptonFB.py
+++ LeptonFB.py
@@ -14,39 +14,42 @@ class LeptonFB(App):
     wid=Widget()
 
     #capture an image from the Lepton sensor 
-    def capture(device = "/dev/spidev0.0"):
+    def capture(flip_v = False , device = "/dev/spidev0.0"):
+
         with Lepton(device) as l:
             a,_ = l.capture()
-        #flip in x axis as image is backwards if not
-        cv2.flip(a,1,a)
         
         #normalise image to take 16 bit range
-        cv2.normalize(a, a, 0, 65535, cv2.NORM_MINMAX)
+        #cv2.normalize(a, a, 0, 65535, cv2.NORM_MINMAX)
         
         #shift so that lower 8 bits contain the most significant bits
-        np.right_shift(a, 8, a)
+        #np.right_shift(a, 8, a)
 
-        return np.uint8(np.rot90(a))
+        return np.uint32(a*2)
 
     #function to add rectangle to screen
     def draw_image(self,wid):
         with wid.canvas:
-                texture = Texture.create(size=(80, 60), colorfmt="rgb")
-                arr = self.capture()
+    	    texture = Texture.create(size=(80, 60), colorfmt="rgb")
+            arr = self.capture()
 
-        arr2 = np.ndarray(shape=[60,80,3],dtype=np.uint8)
-        for x in range(0,80):
-            for y in range(0,60):
-                arr2[y][79-x][0]=arr[x][y]
-                arr2[y][79-x][1]=arr[x][y]
-                arr2[y][79-x][2]=arr[x][y]
+    	    arr2 = np.ndarray(shape=[60,80,3],dtype=np.uint8)
+	    dt=np.dtype((np.uint32,{'r':(np.uint8,0),'g':(np.uint8,1),'b':(np.uint8,2),'a':(np.uint8,3)}))
 
-        data = arr2.tostring() #convert to 8 bit string 
-        texture.blit_buffer(data, bufferfmt="ubyte", colorfmt="rgb")
-        #clear the screen 
-        wid.canvas.clear()
-        #redraw and scale to 600x400 
-        wid.rect = Rectangle(texture=texture, pos=(00,100), size=(600,400))
+    	    for x in range(0,80):
+        	for y in range(0,60):
+		    v = arr[59-y][x].view(dtype=dt)
+            	    arr2[y][x][0]=v['r']
+            	    arr2[y][x][1]=v['g']
+            	    arr2[y][x][2]=v['b']
+
+    	    data = arr2.tostring() #convert to 8 bit string 
+    	    texture.blit_buffer(data, bufferfmt="ubyte", colorfmt="rgb")
+        
+	    #clear the screen 
+    	    wid.canvas.clear()
+    	    #redraw and scale to 600x400 
+    	    wid.rect = Rectangle(texture=texture, pos=(00,100), size=(600,400))
 
     #called when the user presses the exit button
     def exit(self,wid,*largs):
